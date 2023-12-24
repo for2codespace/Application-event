3962a57137b3cbfd6963d58150a6d7bbd21ad73d
exp-of-betrayal <119005542+exp-of-betrayal@users.noreply.github.com>
2023-12-24 13:09:00 +0400

    Update and rename Groupes.py to Groups.py

--- server/resources/Groups.py
+++ server/resources/Groups.py
@@ -3,13 +3,13 @@ from models import GroupList as GroupListTable
 
 
 class Groupes(Resource):
-    path = "/groupes"
+    path = "/groups"
 
     @classmethod
     def get(cls):
         groupes = GroupListTable.get_all()
 
         if groupes:
-            return {"groupes": [g.json() for g in groupes]}, 200
+            return {"groups": [g.json() for g in groupes]}, 200
         else:
-            return {"message": "No groupes found"}, 404
+            return {"message": "No groups found"}, 404
